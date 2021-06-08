8129176f0483a5a865539f846883b44620c399d1
JanSzostakowski <john.szostakowski@hotmail.com>
2021-06-08 20:16:41 +0200

    changing background

--- facerecognition/src/App.js
+++ facerecognition/src/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import Particles from 'react-particles-js';
-import Clarifai from 'clarifai';
 import Navigation from './components/Navigation/Navigation';
 import Signin from './components/Signin/Signin';
 import Register from './components/Register/Register';
@@ -10,9 +9,7 @@ import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import Rank from './components/Rank/Rank';
 import './App.css';
 
-const app = new Clarifai.App({
-  apiKey: 'd88fde6ad1b14540abde1f02e3413421'
-})
+
 const particlesOptions = {
   particles: {
     color: {
@@ -36,10 +33,7 @@ const particlesOptions = {
   }
   }
 
-class App extends Component {
-  constructor() {
-    super();
-    this.state = {
+  const initialState = {
       input: '',
       imageUrl: '',
       box: {},
@@ -52,8 +46,14 @@ class App extends Component {
             entries: 0,
             joined: ''
       }
-    }
   }
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = initialState;
+    } 
+
 
   componentDidMount() {
     fetch('http://localhost:3000/')
@@ -98,10 +98,14 @@ class App extends Component {
 
   onButtonSubmit = () => {
     this.setState({imageUrl: this.state.input})
-    app.models
-      .predict(
-      Clarifai.FACE_DETECT_MODEL,
-      this.state.input)
+      fetch('http://localhost:3000/imageurl', {
+        method: 'post',
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify({
+            input: this.state.input
+        })
+      })
+      .then(response => response.json())
       .then(response => {
         if(response) {
           fetch('http://localhost:3000/image', {
@@ -124,9 +128,7 @@ class App extends Component {
 
   onRouteChange = (route) => {
     if (route === 'signout') {
-      this.setState({isSignedIn: false});
-      this.setState({imageUrl: ''});
-      this.setState({box: {}});
+      this.setState(initialState);
     } else if (route === 'home') {
       this.setState({isSignedIn: true})
     }
